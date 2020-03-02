8cb17bd8f35d894d2021175ac24b9b77f88fa83e
Biswa Karki <biswakarki@biswas-mbp.lan>
2020-03-01 23:18:28 -0500

    therereere

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_launch_configuration" "default" {
   name          = "terraform-springboot-newapp"
   image_id      = "ami-04ff587bc1c1d65e6"
   instance_type = "t2.micro"
-  key_name = "terraform-springboot-newapp"
+  key_name = "terraform-springboot-helloapp"
   security_groups = ["${aws_security_group.default.id}"]
   iam_instance_profile = "${aws_iam_instance_profile.default.name}"
   user_data = "${data.template_file.default.rendered}"
